c67699e7633d23fc05ffd5e402fb759ea3efbd55
Minh Nguyen <nhminh.ng@gmail.com>
2025-09-06 11:29:49 -0500

    feat: Update user

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/auth/userController.js";
+import { registerUser, loginUser, logoutUser, getUser, updateUser } from "../controllers/auth/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/user", protect, getUser);
+router.patch("/user", protect, updateUser);
 export default router;
